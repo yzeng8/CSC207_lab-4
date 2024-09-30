7df574ca3868f37bc2860550c1f8a58d000e0188
oneDotpy <keenardhito@gmail.com>
2024-09-30 10:27:35 -0400

    finished implementing Task 3a

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -28,7 +28,11 @@ public final class GetAverageGradeUseCase {
         // Call the API to get all the grades for the course for all your team members
         // TODO Task 3a: Complete the logic of calculating the average course grade for
         //              your team members. Hint: the getGrades method might be useful.
-
+        String[] members = team.getMembers();
+        for (String member : members) {
+            count++;
+            sum += gradeDataBase.getGrade(member, course).getGrade();
+        }
         if (count == 0) {
             return 0;
         }
